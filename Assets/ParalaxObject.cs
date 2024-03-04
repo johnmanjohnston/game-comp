a48b41484ccb88005c72ba272357b86ae5a90b72
johnmanjohnston <c.ethanjohn@gmail.com>
2024-03-04 23:29:00 +0530

    added parallax

--- Assets/ParalaxObject.cs
+++ Assets/ParalaxObject.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class ParalaxObject : MonoBehaviour
 {
-    // abandoned
+    public float paralaxFactor;
+    private Vector3 startPos;
+
+    private Camera cam;
+
+    public Vector3 offset;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        transform.position = (startPos + (cam.transform.position * paralaxFactor)) + offset;
+    }
 }
